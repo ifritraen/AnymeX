53ec4931f8dcad5fc2e8828660a9a8b0fc83f9c6
ifritraen <ifrit.raen@gmail.com>
2026-07-29 18:18:38 +0600

    fix: resolve compilation errors in library_sync_page and progress_sync_section

--- lib/controllers/sync/progress_sync_section.dart
+++ lib/controllers/sync/progress_sync_section.dart
@@ -519,6 +519,7 @@ class _GistSyncCard extends StatelessWidget {
                                 isLogged: isLogged,
                                 isAuthenticating: ctrl.isAuthenticating.value,
                                 isSyncing: isSyncing,
+                                syncEnabled: ctrl.syncEnabled.value,
                                 hasCloudGist: hasCloudGist,
                                 lastSyncSuccessful: lastSyncSuccessful,
                               ),
--- lib/screens/library/widgets/library_sync_page.dart
+++ lib/screens/library/widgets/library_sync_page.dart
@@ -6,6 +6,7 @@ import 'package:anymex/database/isar_models/custom_list.dart';
 import 'package:anymex/database/isar_models/offline_media.dart';
 import 'package:anymex/models/Media/media.dart';
 import 'package:anymex/utils/theme_extensions.dart';
+import 'package:anymex_extension_runtime_bridge/anymex_extension_runtime_bridge.dart';
 import 'package:anymex/widgets/custom_widgets/anymex_button.dart';
 import 'package:anymex/widgets/custom_widgets/anymex_image.dart';
 import 'package:anymex/widgets/custom_widgets/custom_text.dart';
